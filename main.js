d740e437a499a2027681fb31fb179f5fa57e8d67
AaranReal <94901820+AaranReal@users.noreply.github.com>
2022-07-23 11:25:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+console.log("Hi I am Console Screen");
+var first_name="Aaran";
+console.log(first_name);
+var last_name=" Jowhal";
+console.log(last_name);
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var length=full_name.length;
+console.log(length);
+
+var name_1="I am Jos Butler";
+console.log(name_1);
+var name_replace= name_1.replace("Jos","Rocky");
+console.log(name_replace);
+var capital_name=first_name.toUpperCase();
+console.log(capital_name);
+var lower_name= capital_name.toLowerCase();
+console.log(lower_name);
+
+
+var square_root= Math.sqrt(16);
+console.log(square_root);
+var minimum_number= Math.min(5,8);
+console.log(minimum_number)
+var maximum_number= Math.max(87,99);
+console.log(maximum_number);
+var power= Math.pow(2,4);
+console.log (power);
+
+
